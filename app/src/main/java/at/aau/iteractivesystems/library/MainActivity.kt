d3d5600f96fb2d6f68d6c4ca415544c374212349
= <dominik.lechl@alturos.com>
2022-12-19 16:33:50 +0100

    Initial commit (project created with Activity + Fragment + ViewModel template)

---
+++ app/src/main/java/at/aau/iteractivesystems/library/MainActivity.kt
@@ -0,0 +1,18 @@
+package at.aau.iteractivesystems.library
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import at.aau.iteractivesystems.library.ui.main.MainFragment
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        if (savedInstanceState == null) {
+            supportFragmentManager.beginTransaction()
+                .replace(R.id.container, MainFragment.newInstance())
+                .commitNow()
+        }
+    }
+}
