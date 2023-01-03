4af3221fd98fe92b335b1e85ff305cbd480d0a12
lechld <68105512+lechld@users.noreply.github.com>
2023-01-03 15:14:11 +0100

    Fix nested RecyclerView state not restored properly (#5)
    Co-authored-by: Dominik Lechl <dominik.lechl@alturos.com>

--- app/src/main/java/at/aau/iteractivesystems/library/ui/adapter/viewholder/SectionViewHolder.kt
+++ app/src/main/java/at/aau/iteractivesystems/library/ui/adapter/viewholder/SectionViewHolder.kt
@@ -3,21 +3,34 @@ package at.aau.iteractivesystems.library.ui.adapter.viewholder
 import androidx.recyclerview.widget.RecyclerView
 import at.aau.iteractivesystems.library.databinding.ItemSectionBinding
 import at.aau.iteractivesystems.library.ui.adapter.Content
+import at.aau.iteractivesystems.library.ui.adapter.staterestoration.NestedRecyclerViewViewHolder
 import at.aau.iteractivesystems.library.ui.adapter.viewholder.section.BigSectionAdapter
 import at.aau.iteractivesystems.library.ui.adapter.viewholder.section.SmallSectionAdapter
 
 class SectionViewHolder(
     private val binding: ItemSectionBinding,
     onClick: (Content.Section.Item) -> Unit,
-) : RecyclerView.ViewHolder(binding.root) {
+) : RecyclerView.ViewHolder(binding.root), NestedRecyclerViewViewHolder {
+
+    private var section: Content.Section? = null
 
     private val bigAdapter = BigSectionAdapter(onClick)
     private val smallAdapter = SmallSectionAdapter(onClick)
 
     fun bind(item: Content.Section) {
+        this.section = item
+
         val adapter = if (item is Content.Section.Big) bigAdapter else smallAdapter
 
         binding.recycler.adapter = adapter
         adapter.submitList(item.items)
+    }
+
+    override fun getId(): String {
+        return section?.id ?: throw IllegalStateException("Section is null!")
+    }
+
+    override fun getLayoutManager(): RecyclerView.LayoutManager? {
+        return binding.recycler.layoutManager
     }
 }
