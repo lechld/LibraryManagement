6b05b7433b4932653b4c4aa710a09dab9d5f33b4
Dominik Lechl <dominik.lechl@alturos.com>
2023-01-14 11:32:18 +0100

    Move search view only inside explore screen

--- app/src/main/java/at/aau/iteractivesystems/library/ui/main/MainActivity.kt
+++ app/src/main/java/at/aau/iteractivesystems/library/ui/main/MainActivity.kt
@@ -1,12 +1,8 @@
 package at.aau.iteractivesystems.library.ui.main
 
-import android.content.res.ColorStateList
 import android.os.Bundle
 import android.view.MenuItem
 import androidx.appcompat.app.AppCompatActivity
-import androidx.appcompat.widget.SearchView
-import androidx.core.content.res.ResourcesCompat
-import androidx.core.view.isVisible
 import androidx.fragment.app.FragmentContainerView
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
@@ -17,9 +13,6 @@ import at.aau.interactivesystems.library.EnvironmentImpl
 import at.aau.iteractivesystems.library.R
 import at.aau.iteractivesystems.library.ViewModelFactory
 import at.aau.iteractivesystems.library.databinding.ActivityMainBinding
-import at.aau.iteractivesystems.library.ui.main.search.SearchDialogFragment
-import at.aau.iteractivesystems.library.ui.main.search.SearchTextViewModel
-import com.google.android.material.color.MaterialColors
 
 /**
  * That's the main and startup activity.
@@ -37,14 +30,6 @@ class MainActivity : AppCompatActivity() {
         ViewModelProvider(this, ViewModelFactory(EnvironmentImpl))
     }
 
-    private val searchTextViewModel by lazy {
-        viewModelFactory[SearchTextViewModel::class.java]
-    }
-
-    private val floatingActionViewModel by lazy {
-        viewModelFactory[FloatingActionViewModel::class.java]
-    }
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
@@ -52,8 +37,6 @@ class MainActivity : AppCompatActivity() {
         setContentView(binding.root)
 
         setupNavigation(binding)
-        setupSearch(binding)
-        setupFloatingAction(binding)
 
         this.binding = binding
     }
@@ -69,59 +52,5 @@ class MainActivity : AppCompatActivity() {
         val controller = navHost.navController
 
         binding.navigationView.setupWithNavController(controller)
-    }
-
-    private fun setupSearch(binding: ActivityMainBinding) {
-        binding.searchView.setOnQueryTextListener(object : SearchView.OnQueryTextListener {
-            override fun onQueryTextSubmit(query: String?): Boolean {
-                searchTextViewModel.setQuery(query)
-                if (!query.isNullOrEmpty()) {
-                    SearchDialogFragment.show(supportFragmentManager)
-                    binding.searchView.clearFocus() // Avoid auto focus on search view when dialog is closed
-                }
-                return false
-            }
-
-            override fun onQueryTextChange(newText: String?): Boolean {
-                return false
-            }
-        })
-
-        searchTextViewModel.query.observe(this) { query ->
-            binding.searchView.setQuery(query, false)
-        }
-    }
-
-    private fun setupFloatingAction(binding: ActivityMainBinding) {
-        val addIcon = ResourcesCompat.getDrawable(resources, R.drawable.add, theme)
-        val removeIcon = ResourcesCompat.getDrawable(resources, R.drawable.remove, theme)
-        val addColor = MaterialColors.getColor(
-            binding.root, com.google.android.material.R.attr.colorPrimaryInverse
-        )
-        val removeColor = MaterialColors.getColor(
-            binding.root, com.google.android.material.R.attr.colorTertiary
-        )
-
-        floatingActionViewModel.action.observe(this) { action ->
-            when (action) {
-                FloatingActionViewModel.Action.Add -> {
-                    binding.fab.backgroundTintList = ColorStateList.valueOf(addColor)
-                    binding.fab.setImageDrawable(addIcon)
-                    binding.fab.isVisible = true
-                }
-                FloatingActionViewModel.Action.Hidden -> {
-                    binding.fab.isVisible = false
-                }
-                FloatingActionViewModel.Action.Remove -> {
-                    binding.fab.backgroundTintList = ColorStateList.valueOf(removeColor)
-                    binding.fab.setImageDrawable(removeIcon)
-                    binding.fab.isVisible = true
-                }
-            }
-        }
-
-        binding.fab.setOnClickListener {
-            floatingActionViewModel.setSelected()
-        }
     }
 }
